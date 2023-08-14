8faef21ef7c9cf66dac508a01c15812f6161e00d
wx <2943999149@qq.com>
2023-08-14 10:57:27 +0800

    Automated commit

--- push_changes.sh
+++ push_changes.sh
@@ -4,14 +4,14 @@ LOGFILE="./log/push_changes_log/logfile.log"
 
 # cd /path/to/your/repo
 
-echo "Script started at $(date)" | tee -a $LOGFILE
+echo "[ Script started at $(date) ]" | tee -a $LOGFILE
 
-git add .
+git add . 2>&1 | tee -a $LOGFILE
 
-git commit -m "Automated commit"
+git commit -m "Automated commit" 2>&1 | tee -a $LOGFILE
 
 git push 2>&1 | tee -a $LOGFILE
 
-echo "Script ended at $(date)" | tee -a $LOGFILE
+echo "[ Script ended at $(date) ]\n" | tee -a $LOGFILE
 
 echo "Changes pushed and log saved to $LOGFILE"
