98589c0a5ffaf024ed24efaa5a31cd21903e7801
wx <2943999149@qq.com>
2023-08-31 10:25:47 +0800

    Automated commit

--- push_changes.sh
+++ push_changes.sh
@@ -5,13 +5,13 @@ TMPFILE="./log/push_changes_log/logfiletmp.log"
 
 # cd /path/to/your/repo
 
-echo "[>>> Start : $(date) <<<]" | tee -a $LOGFILE
+echo "[>>> Start : $(date) <<<]" | tee -a $TMPFILE
 
-git add . 2>&1 | tee -a $LOGFILE
+git add . 2>&1 | tee -a $TMPFILE
 
-git commit -m "Automated commit" 2>&1 | tee -a $LOGFILE
+git commit -m "Automated commit" 2>&1 | tee -a $TMPFILE
 
-git push 2>&1 | tee -a $LOGFILE
+git push 2>&1 | tee -a $TMPFILE
 
 echo "[>>> End : $(date) <<<]" | tee -a $TMPFILE
 echo "" >> $TMPFILE
